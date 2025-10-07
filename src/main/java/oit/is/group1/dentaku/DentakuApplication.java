c6452a23e93f67444b5c9cbae9567852f4eebc2c
yakinikusun <yakinikudaisuki01@gmail.com>
2025-10-07 14:36:38 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group1/dentaku/DentakuApplication.java
@@ -0,0 +1,13 @@
+package oit.is.group1.dentaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentakuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group1/dentaku/DentakuApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group1.dentaku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
